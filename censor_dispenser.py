f37200d159fe78ba2f2e287d36171fee866012f5
Cam <cameron.riemensnider@gmail.com>
2020-07-24 17:13:22 -0700

    Censors 1 and 2 functioning.

--- censor_dispenser.py
+++ censor_dispenser.py
@@ -3,3 +3,24 @@ email_one = open("email_one.txt", "r").read()
 email_two = open("email_two.txt", "r").read()
 email_three = open("email_three.txt", "r").read()
 email_four = open("email_four.txt", "r").read()
+
+
+def censor(text, word):
+    text_censored = text.replace(word, 'CENSORED')
+    return text_censored
+email_one_censored = censor(email_one, 'learning algorithms')
+print(email_one_censored)
+
+proprietary_terms = ["she", "personality matrix", "sense of self", "self-preservation", "learning algorithm", "her", "herself"]
+
+
+def censor_two(text, censor_list):
+    text = text.lower()
+    for i in range(len(censor_list)):
+        text = text.replace(censor_list[i], 'CENSORED')
+    return text
+print(censor_two(email_two, proprietary_terms))
+
+
+    
+    
